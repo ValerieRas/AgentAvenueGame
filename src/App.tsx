e2aa98adea83fdd2f08ee90c1a8a7ca9f2f5869d
valerieRas <rasolovalerie@gmail.com>
2026-01-27 12:17:18 +0100

    feat:got carte component and added that to joueur

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 
 import './App.css';
-import Plateau from './Components/Plateau/Plateau';
+import { Carte } from './Components/Carte/Carte';
+import { listeCartes } from './datas/cartes';
+import { Joueur } from './Components/Joueur/Joueur';
 
 function App() {
+
+  /*const [selectionneeIndex, setSelectionneeIndex] = useState<number | null>(null); */
+
   return (
     <div className="App">
-      <Plateau
-        joueurDeuxPosition={2}
-        joueurUnPosition={5}
-        showValues={false}
-        imageFondUrl="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FydG9vbiUyMGJvYXJkfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60"
-      />
+      {/* {listeCartes.map((carte, index) => 
+        <Carte 
+          key={index} 
+          {...carte} 
+          selectionnee={index === selectionneeIndex}
+          onClick={() => setSelectionneeIndex(index)} 
+        />
+      )} */}
+
+      <div className="zone-jeu"></div>
+
+      <div className="conteneur-joueur">
+
+      <Joueur id="1" name="Joueur 1" hand={[listeCartes[0], listeCartes[1], listeCartes[2], listeCartes[3]]} gainedCards={[]}/>
+
+      <div className="plateau"></div>
+
+      <Joueur id="2" name="Joueur 2" hand={[listeCartes[4], listeCartes[5], listeCartes[6], listeCartes[7]]} gainedCards={[]}/>
+      </div>
     </div>
   )
 }
--- src/Components/Joueur/Joueur.tsx
+++ src/Components/Joueur/Joueur.tsx
@@ -1,22 +1,48 @@
-import {Carte} from "../Carte";
+import { useState, useEffect } from "react";
+import { Carte} from "../Carte/Carte";
+import type { CarteProps } from "../Carte/Carte";
+import { listeCartes } from "../../datas/cartes";
+import "./joueur.css"; 
 
-export function Joueur() {
+interface JoueurProps {
+  id:string;
+  name: string;
+  hand: CarteProps[];
+  gainedCards : CarteProps[];
+}
 
-    
+export function Joueur(
+    props: JoueurProps
+) {
+    const [selectionneeIndex, setSelectionneeIndex] = useState<number>(-1);
 
   return (
-    <div className="zone-joueur">
-      
-      {/* 1. The Main Hand (4 Cards) */}
+    <div className="zone-joueur" id={props.id}>
+
+      <h2 className="joueur-nom">{props.name}</h2>
+
       <div className="main">
-        <Carte />
-        <Carte />
-        <Carte />
-        <Carte />
+        {props.hand.map((carte, index) => (
+          <Carte 
+            key={index} 
+            {...carte} 
+            selectionnee={index === selectionneeIndex}
+            onClick={() => setSelectionneeIndex(index)} 
+          />
+        ))}
       </div>
 
+      <h2 className="agent-titre">Agents Recrutés</h2>
+
       <div className="carte-gagnee">
-  
+            {props.gainedCards.map((carte, index) => (
+          <Carte 
+            key={index} 
+            {...carte} 
+            selectionnee={index === selectionneeIndex}
+            onClick={() => setSelectionneeIndex(index)} 
+          />
+        ))}
       </div>
 
     </div>
