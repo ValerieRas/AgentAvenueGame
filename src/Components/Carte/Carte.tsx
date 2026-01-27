be11a78752b24fe82d69f23d27f87eb0e05b7489
ThomasLaforge <thomas.laforge.38@gmail.com>
2026-01-27 09:27:58 +0100

    composant Carte

--- src/Components/Carte/Carte.tsx
+++ src/Components/Carte/Carte.tsx
@@ -2,11 +2,26 @@ export interface CarteProps {
   imageUrl?: string,
   nom: string,
   couleur: string,
-  scores: number[]
+  scores: number[],
+  selectionnee?: boolean
 }
 
 export function Carte(
-
+  props: CarteProps
 ) {
-  return 
+  return <div className="carte"
+    style={{backgroundColor: props.couleur,
+      position: "relative",
+      top: props.selectionnee ? "-10px" : "0px",
+    }}
+  >
+    <h4 className="carte-nom">{props.nom}</h4>
+    <div className="carte-scores">
+      {props.scores.map((score, index) =>
+        <div key={index} className="carte-score">
+          {score}
+        </div>
+      )}
+    </div>
+  </div>
 }
