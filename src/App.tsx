f8185e31d9f67ab5b1cd776ac96677e461ac9dd0
valerieRas <rasolovalerie@gmail.com>
2026-01-27 15:27:18 +0100

    feat: included Plateau.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 
 import './App.css';
-import { Carte } from './Components/Carte/Carte';
 import { listeCartes } from './datas/cartes';
 import { Joueur } from './Components/Joueur/Joueur';
+import Plateau from './Components/Plateau/Plateau';
 
 function App() {
 
@@ -23,11 +23,23 @@ function App() {
 
       <div className="conteneur-joueur">
 
-      <Joueur id="1" name="Joueur 1" hand={[listeCartes[0], listeCartes[1], listeCartes[2], listeCartes[3]]} gainedCards={[]}/>
+      <Joueur id="1" name="Joueur 1" 
+      hand={[listeCartes[0], listeCartes[1], listeCartes[2], listeCartes[3]]} 
+      carteGagnee={[]}
+      carteSelectionee={[]}
+      />
 
-      <div className="plateau"></div>
+      <Plateau
+        joueurUnPosition={0}
+        joueurDeuxPosition={7}
+        showValues={true}
+        imageFondUrl="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l0eSUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
+      ></Plateau>
 
-      <Joueur id="2" name="Joueur 2" hand={[listeCartes[4], listeCartes[5], listeCartes[6], listeCartes[7]]} gainedCards={[]}/>
+      <Joueur id="2" name="Joueur 2" 
+      hand={[listeCartes[4], listeCartes[5], listeCartes[6], listeCartes[7]]} 
+      carteGagnee={[]}
+      carteSelectionee={[]}/>
       </div>
     </div>
   )
